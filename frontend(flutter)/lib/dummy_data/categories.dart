56848040355a060fec0f6c8557548df4e4668c2e
nitin-sai-geon <nitin.sai@gadgeon.com>
2026-06-09 16:08:21 +0530

    Delete frontend(flutter) directory

--- frontend(flutter)/lib/dummy_data/categories.dart
+++
@@ -1,6 +0,0 @@
-List<Map<String, dynamic>> categories = [
-  {'id': 'A1A1A1A1-A1A1-A1A1-A1A1-A1A1A1A1A1A1', 'name': 'Salary', 'type': 0},
-  {'id': 'B2B2B2B2-B2B2-B2B2-B2B2-B2B2B2B2B2B2', 'name': 'Food', 'type': 1},
-  {'id': 'C3C3C3C3-C3C3-C3C3-C3C3-C3C3C3C3C3C3', 'name': 'Transport', 'type': 1},
-  {'id': 'D4D4D4D4-D4D4-D4D4-D4D4-D4D4D4D4D4D4', 'name': 'Entertainment', 'type': 1},
-];
--- frontend(flutter)/lib/dummy_data/users.dart
+++
@@ -1,8 +0,0 @@
-List<Map<String, dynamic>> users = [
-  {
-    'email': 'nitinsai@example.com',
-    'password': 'nitin123',
-    'name': 'Nitin Sai',
-  },
-  {'email': 'admin@example.com', 'password': 'admin123', 'name': 'Admin User'},
-];
